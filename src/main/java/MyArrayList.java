fb7720fe9333f6673b385f789cf2d89f4191c681
vlad-1308 <vskrasnik@yandex.ru>
2022-02-09 13:55:45 +0300

    Just added methods

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,2 +1,62 @@
-public class MyArrayList implements MyList {
+public class MyArrayList<T> implements MyList {
+
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public void add(Object o, int index) {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void addAll(MyList newList) {
+
+    }
+
+    @Override
+    public boolean delete(int i) {
+        return false;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean clear() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
 }
--- src/main/java/MyList.java
+++ src/main/java/MyList.java
@@ -1,2 +1,30 @@
-public interface MyList {
+public interface MyList<T> {
+
+    // void add(Object o);
+
+    void add(T t);
+
+    void add(Object o, int index);
+
+    void sort();
+
+    //Same as concat();
+    void addAll(MyList<T> newList);
+
+    boolean delete(int i);
+
+    T get(int index);
+
+    int size();
+
+    int indexOf(T t);
+
+    T[] toArray();
+
+    boolean isEmpty();
+
+    boolean clear();
+
+    boolean contains(Object o);
+
 }
