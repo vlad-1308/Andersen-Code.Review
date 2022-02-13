ab819c1fd017a5c38ad69d6c561722374e925f2c
vlad-1308 <vskrasnik@yandex.ru>
2022-02-13 16:52:31 +0300

    Add implementation

--- src/main/java/myLinkedList/MyLinkedList.java
+++ src/main/java/myLinkedList/MyLinkedList.java
@@ -1,10 +1,78 @@
 package myLinkedList;
 
+import myArrayList.MyList;
+
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.LinkedList;
 
-public class MyLinkedList {
+public class MyLinkedList<T> implements MyList<T> {
 
 
+    @Override
+    public void add(T t) {
+
+    }
+
+    @Override
+    public void add(T t, int index) {
+
+    }
+
+    @Override
+    public void sort(Comparator<? super T> comparator) {
+
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> newList, int index) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> newList) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(int i) {
+        return false;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public int indexOf(T t) {
+        return 0;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(T t) {
+        return false;
+    }
 
     private static class MyNode<E> {
         E item;
